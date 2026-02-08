80334ba16aa975c8f4a5e9d11c855707ef9ae7e3
versys-coder <75164354+versys-coder@users.noreply.github.com>
2026-02-08 14:13:00 +0500

    Remove unused ModeSwitch component

--- frontend/src/ModeSwitch.tsx
+++
@@ -1,9 +0,0 @@
-export default function ModeSwitch() {
-  return (
-    <div className="mode-switch">
-      <button className="ui-pill active" type="button">
-        Вариант 03_20_2026
-      </button>
-    </div>
-  );
-}
